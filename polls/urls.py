9b275b2d39cac286e9e007f9196d5a8b07427a09
frwong <frwong@hotmail.com>
2016-11-25 11:47:56 +0800

    Fisnished Lesson 5

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from . import views
 
-
+'''app_name='polls'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+   url(r'^$', views.index, name='index'),
 	 # ex: /polls/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
@@ -12,4 +12,13 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
     # added the word 'specifics'
 	url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+
+]'''
+
+app_name = 'polls'
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
